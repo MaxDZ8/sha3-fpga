5c5d497dfbe4311906b4dac7fe2013f0e0161567
MaxDZ8 <MassimoDZ8@gmail.com>
2020-06-06 19:11:08 +0200

    Fix single driver for good, now based on the state capturer to be more compact.

--- rtl/sha3_state_delayer.sv
+++ rtl/sha3_state_delayer.sv
@@ -2,7 +2,7 @@
 
 // A bunch of buffers to delay 25 ulongs a few clocks!
 module sha3_state_delayer #(
-    DELAY = 4
+    unsigned DELAY = 4
 )(
     input clk,
     // input slices
@@ -29,57 +29,32 @@ else if(DELAY == 0) begin
     assign good = sample;
 end
 else if(DELAY == 1) begin : just_buffin // Buffer the inputs.
-    longint unsigned buffin[5][5];
-    bit goodbuff = 1'b0;
-    for (comp = 0; comp < 5; comp++) begin
-        always_ff @(posedge clk) if (sample) begin
-            buffin[0][comp] <= isa[comp];
-            buffin[1][comp] <= isb[comp];
-            buffin[2][comp] <= isc[comp];
-            buffin[3][comp] <= isd[comp];
-            buffin[4][comp] <= ise[comp];
-        end
-        assign oda[comp] = buffin[0][comp];
-        assign odb[comp] = buffin[1][comp];
-        assign odc[comp] = buffin[2][comp];
-        assign odd[comp] = buffin[3][comp];
-        assign ode[comp] = buffin[4][comp];
-    end
-    always_ff @(posedge clk) goodbuff <= sample;
-    assign good = goodbuff;
+    sha3_state_delayer delayin(
+        .clk(clk),
+        .sample(sample), .isa(isa), .isb(isb), .isc(isc), .isd(isd), .ise(ise),
+        .ogood(good),
+        .osa(oda), .osb(odb), .osc(odc), .osd(odd), .ose(ode)
+    );
 end
-else if(DELAY == 2) begin : iobuff // copy input buffer to output buffer
-    bit[1:0] delayed_good = 2'b0;
-    always_ff @(posedge clk) delayed_good <= { delayed_good[0], sample };
-    assign good  = delayed_good[1];
-    
-    longint unsigned buffin[5][5];
-    longint unsigned buffout[5][5];
-    for (comp = 0; comp < 5; comp++) begin
-        always_ff @(posedge clk) if (sample) begin
-            buffin[0][comp] <= isa[comp];
-            buffin[1][comp] <= isb[comp];
-            buffin[2][comp] <= isc[comp];
-            buffin[3][comp] <= isd[comp];
-            buffin[4][comp] <= ise[comp];
-        end
-        always_ff @(posedge clk) begin
-            buffout[0][comp] <= buffin[0][comp];
-            buffout[1][comp] <= buffin[1][comp];
-            buffout[2][comp] <= buffin[2][comp];
-            buffout[3][comp] <= buffin[3][comp];
-            buffout[4][comp] <= buffin[4][comp];
-        end
-        assign oda[comp] = buffout[0][comp];
-        assign odb[comp] = buffout[1][comp];
-        assign odc[comp] = buffout[2][comp];
-        assign odd[comp] = buffout[3][comp];
-        assign ode[comp] = buffout[4][comp];
-    end
+else if (DELAY == 2) begin : iobuff // copy input buffer to output buffer
+    wire[63:0] between[5][5];
+    wire delayed;
+    sha3_state_capture delay_before(
+        .clk(clk),
+        .sample(sample), .isa(isa), .isb(isb), .isc(isc), .isd(isd), .ise(ise),
+        .ogood(delayed),
+        .osa(between[0]), .osb(between[1]), .osc(between[2]), .osd(between[3]), .ose(between[4])
+    );
+    sha3_state_capture delay_later(
+        .clk(clk),
+        .sample(delayed),
+        .isa(between[0]), .isb(between[1]), .isc(between[2]), .isd(between[3]), .ise(between[4]),
+        .ogood(good),
+        .osa(oda), .osb(odb), .osc(odc), .osd(odd), .ose(ode)
+    );
 end
-else begin // generic delay, one input, one output and something between
+else begin : generically // one input, one output and something between
     longint unsigned buff[DELAY][5][5];
-    genvar lateness;
     for (comp = 0; comp < 5; comp++) begin
         // let's start with the inputs.
         always_ff @(posedge clk) if (sample) begin
@@ -90,7 +65,7 @@ else begin // generic delay, one input, one output and something between
             buff[0][4][comp] <= ise[comp];
         end
         // the in-between shift
-        for (lateness = 0; lateness < DELAY - 1; lateness++) begin
+        for (genvar lateness = 0; lateness < DELAY - 1; lateness++) begin
             always_ff @(posedge clk) begin
                 buff[lateness + 1][0][comp] <= buff[lateness][0][comp]; 
                 buff[lateness + 1][1][comp] <= buff[lateness][1][comp]; 
@@ -104,11 +79,11 @@ else begin // generic delay, one input, one output and something between
         assign odc[comp] = buff[DELAY - 1][2][comp];
         assign odd[comp] = buff[DELAY - 1][3][comp];
         assign ode[comp] = buff[DELAY - 1][4][comp];
-
-        bit[(DELAY - 1):0] delayed_good = 0;
-        always_ff @(posedge clk) delayed_good <= { delayed_good[(DELAY - 2):0], sample };
-        assign good  = delayed_good[DELAY - 1];
     end
+
+    bit[(DELAY - 1):0] delayed_good = 0;
+    always_ff @(posedge clk) delayed_good <= { delayed_good[(DELAY - 2):0], sample };
+    assign good  = delayed_good[DELAY - 1];
 end
 
 endmodule
