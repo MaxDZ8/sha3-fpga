9e9232deda0987f392e5b061969b35d304c35a63
MaxDZ8 <MassimoDZ8@gmail.com>
2020-06-07 21:25:59 +0200

    Initial test of the roller. The testbench runs but must be re-checked. NOT WORKING

---
+++ rtl/sha3_scanner.sv
@@ -0,0 +1,116 @@
+`timescale 1ns / 1ps
+
+module sha3_scanner #(
+    string THETA_UPDATE_LOGIC_STYLE = "basic",
+    string CHI_MODIFY_STYLE = "basic",
+    string IOTA_STYLE = "basic"
+)(
+    input clk,
+    input start,
+    
+    // "Difficulty"
+    input[63:0] threshold,
+    // Builds 12 ulongs where the i-th ulong is { blobby[i*2 + 1], blobby[i * 2] }
+    // blobby[20] is special: start nonce
+    // Only a part of the inital state is to be provided, the other bits are built internally (and most are constant anyway)
+	  input[31:0] blobby[24],
+	  
+	  output dispatching, evaluating, found, ready,
+	  output[31:0] nonce,
+	  output[63:0] hash[25]
+);
+
+wire capture = start & ready;
+longint unsigned rowa[5], rowb[5]; // those are captured from blobby input, completely defined.
+longint unsigned rowc[2]; // only two entries in the third row are defined by block input, the others are magic or constants
+always_ff @(posedge clk) if(capture) begin
+    rowa[0] <= { blobby[ 1], blobby[ 0] };
+    rowa[1] <= { blobby[ 3], blobby[ 2] };
+    rowa[2] <= { blobby[ 5], blobby[ 4] };
+    rowa[3] <= { blobby[ 7], blobby[ 5] };
+    rowa[4] <= { blobby[ 9], blobby[ 8] };
+    rowb[0] <= { blobby[11], blobby[10] };
+    rowb[1] <= { blobby[13], blobby[12] };
+    rowb[2] <= { blobby[15], blobby[14] };
+    rowb[3] <= { blobby[17], blobby[15] };
+    rowb[4] <= { blobby[19], blobby[18] };
+    rowc[0] <= { blobby[21], blobby[20] };
+    rowc[1] <= { blobby[23], blobby[22] };
+end
+
+wire[31:0] scan_start = rowc[0][63:32];
+int unsigned dispatch_iterator = 32'b0, next_nonce = 32'b0;
+bit buff_dispatching = 1'b0;
+always_ff @(posedge clk) if (buff_dispatching) begin
+    buff_dispatching <= dispatch_iterator < 32'hFFFFFFFF & ~found;
+    dispatch_iterator <= dispatch_iterator + 1'b1;
+    next_nonce <= next_nonce + 1;
+end
+else begin
+    buff_dispatching <= capture;
+    dispatch_iterator <= 32'b0;
+    next_nonce <= scan_start;
+end
+assign dispatching = buff_dispatching;
+
+
+wire[63:0] rowc2 = {
+    32'h00000006, // this is block finalization from SHA3, it should be the whole ulong ^ 64'h00000006_00000000 but I cut it easy 
+    next_nonce
+};
+
+localparam longint unsigned rowc_final[2] = '{ 64'h0, 64'h0 };
+localparam longint unsigned rowd_final[5] = '{ 64'h0, 64'h0, 64'h80000000_00000000, 64'h0, 64'h0 };
+localparam longint unsigned rowe_final[5] = '{ 64'h0, 64'h0, 64'h0, 64'h0, 64'h0 };
+
+wire[63:0] resa[5], resb[5], resc[5], resd[5], rese[5];
+sha3 hasher(
+    .clk(clk),
+    .isa(rowa), .isb(rowb),
+    .isc('{ rowc[0], rowc[1], rowc2, rowc_final[0], rowc_final[1] }),
+    .isd('{ rowd_final[0], rowd_final[1], rowd_final[2], rowd_final[3], rowd_final[4] }),
+    .ise('{ rowe_final[0], rowe_final[1], rowe_final[2], rowe_final[3], rowe_final[4] }),
+    .sample(buff_dispatching),
+    .osa(resa), .osb(resb), .osc(resc), .osd(resd), .ose(rese),
+    .ogood(evaluating)
+);
+
+wire[63:0] hash_diff = { resa[0][7:0], resa[0][15:8], resa[0][23:16], resa[0][31:24], resa[0][39:32], resa[0][47:40], resa[0][55:48], resa[0][63:54] };
+wire good_enough = evaluating & hash_diff < threshold;
+
+int unsigned result_iterator = 32'b0;
+bit buff_found = 1'b0;
+always_ff @(posedge clk) begin
+    if (ready) begin
+        if (capture) begin
+            buff_found <= 1'b0;
+            result_iterator <= 32'b0;
+        end
+    end
+    else buff_found <= buff_found | good_enough;
+    result_iterator <= evaluating;
+end
+assign found = buff_found;
+
+int unsigned good_nonce = 32'b0;
+longint unsigned good_hash[25];
+always_ff @(posedge clk) if(good_enough) begin
+    good_nonce <= result_iterator;
+    good_hash <= '{
+        resa[0], resa[1], resa[2], resa[3], resa[4],
+        resb[0], resb[1], resb[2], resb[3], resb[4],
+        resc[0], resc[1], resc[2], resc[3], resc[4],
+        resd[0], resd[1], resd[2], resd[3], resd[4],
+        rese[0], rese[1], rese[2], rese[3], rese[4]
+    };
+end
+assign nonce = good_nonce;
+for (genvar loop = 0; loop < 25; loop++) assign hash[loop] = good_hash[loop];
+
+
+bit was_evaluating = 1'b0;
+always_ff @(posedge clk) was_evaluating <= evaluating;
+wire empty_pipeline = was_evaluating & ~evaluating;
+assign ready = result_iterator == dispatch_iterator;
+
+endmodule
---
+++ vivado/sha3scanner_axi_device/sha3scanner_project/sha3scanner_project.srcs/uut_sha3_scanner/new/uut_sha3_scanner.sv
@@ -0,0 +1,111 @@
+`timescale 1ns / 1ps
+
+module uut_sha3_scanner();
+
+localparam CLOCK_RATE = 100_000_000;
+localparam PERIOD = 1_000_000_000.0 / CLOCK_RATE;
+
+bit clk = 1'b0, reset = 1, dispatching = 1'b0;
+
+initial begin
+  clk = 0;
+  forever begin
+    #(PERIOD/2.0) clk = ~clk;
+  end
+end
+
+// This represents a work->data from legacy miners, where it is usually uint[48].
+// For SHA3, only the first 24 values are used, the others can be trashed.
+// Entry [21] is special because it is the 'scan start'. It works in a weird way, it's not just a nonce increment
+// but also embedded in the block itself so it is not really an offset as hash is a non-linear function.
+// Scan-hash in this block is 0. All others are 1024*1024+i.
+localparam int unsigned block_template[24] = '{
+    32'h00100000, 32'h00100001, 32'h00100002, 32'h00100003, 32'h00100004, 32'h00100005, 32'h00100006, 32'h00100007,
+    32'h00100008, 32'h00100009, 32'h0010000a, 32'h0010000b, 32'h0010000c, 32'h0010000d, 32'h0010000e, 32'h0010000f,
+    32'h00100010, 32'h00100011, 32'h00100012, 32'h00100013, 32'h00100014, 32'h00000000, 32'h00100016, 32'h00100017
+};
+
+localparam string test_mode = "short";
+
+// In legacy miners this is usually work->target[6,7]. They are two uints, loaded as an ulong.
+// Now if each uint is assigned 64*1024*1024 + 1024+i you get those magic values.
+// This will find a nonce in 56 scans.
+localparam longint unsigned short_threshold = 64'h0400040704000406;
+
+// A lower difficulty results in less nonces found. Here you will need to scan the above block template
+// until testing nonce 61855. It's quite excessive for behavioural simulation.
+localparam longint unsigned long_threshold = 64'h0007000000060000;
+
+
+localparam longint unsigned threshold = test_mode == "short" ? short_threshold : long_threshold;
+
+bit start = 1'b0, pulsed = 1'b0;
+
+wire scanning, evaluating, found, scanner_ready;
+wire[31:0] good_enough;
+wire[63:0] result[25];
+
+sha3_scanner scanner(
+    .clk(clk), .start(start),
+    .threshold(threshold),
+	  .blobby(block_template),
+	  
+	  .dispatching(scanning), .evaluating(evaluating), .found(found), .ready(scanner_ready),
+	  .nonce(good_enough),
+	  .hash(result)
+);
+
+
+initial begin
+  $timeformat(-9,2," ns",14);
+  $display("testbench start SHA3-1600 scanner");
+  #150 // wait for GSR and other nonsense.
+  reset = 0;
+  #50
+  $display("signals considered settled");
+  dispatching = 1;
+end
+  
+always_ff @(posedge clk) begin
+    if (dispatching) begin
+        start <= ~start & ~pulsed;
+        if (!pulsed) pulsed <= 1'b1;
+    end
+end
+
+localparam longint unsigned short_hash[25] = '{
+    64'hd1a8bd39a62c7301, 64'h0c27714aa1d395c4, 64'hbb1c1c5c70f6246a, 64'h33c1bd22d85e31dc, 64'h2687dc79a0864a5f,
+    64'hd841d964ba7aeb5e, 64'h3f3cd2e460a055f4, 64'h272ee53c700af7a4, 64'h52b9090fd15778b6, 64'hb38235c32d28d9c3,
+    64'h9816ea680c109ebe, 64'h24fa8d19b3b32462, 64'hd1b0d2ac6cee1e22, 64'ha4abd968b969d2b9, 64'hff329e31d83cda62,
+    64'h325b8d98168382a5, 64'h96a8aed4a23e081a, 64'h30934e6194879742, 64'hee9538aa987e326c, 64'h2ae78d38d38203dc,
+    64'h7e4710a4891383f6, 64'h72c6aa131bd18fd6, 64'h67cb52177f28e29a, 64'h90971806cef30b2a, 64'hf94ed2f472ea6f59
+};
+
+
+localparam longint unsigned long_hash[25] = '{
+    64'h8c6497333bf0600, 64'h3f0191abab17029, 64'ha4e95e1d037ee98, 64'h03f3c3a05bae7ba, 64'h1ace62e0314fd98,
+    64'h6eba51026524539, 64'ha7b72892679212f, 64'h0e08f9fad233445, 64'h7c6c8bc84070a32, 64'he13ff443162a018,
+    64'h32a7af4473da4a2, 64'hcfaf557cdc5ab9d, 64'h2f5fecce8b864cb, 64'h6af9c1b1a2f217f, 64'h1bf3838216a029f,
+    64'habb45c48e248076, 64'hfe5664f4d03a4b0, 64'h3d5a56330548eef, 64'h7a258259bb0d2cd, 64'h1ae69eeed21744d,
+    64'h0fd711ebcc5d45f, 64'h9fcebe5166ab01e, 64'ha574eed4d6df9ae, 64'hb6efcd50e6d2e80, 64'hc9818df1d4f8128
+};
+
+function expected_result(input int index);
+    if(test_mode == "short") return short_hash[index];
+    return long_hash[index];
+endfunction
+
+always @(posedge clk) if(found) begin
+  for (int loop = 0; loop < 25; loop++) begin
+      if (result[loop] != expected_result(loop)) begin
+        $display("Result[%d] !! FAILED !! (expected %h, found %h)", loop, expected_result(loop), result[loop]);
+        $fatal;
+        $finish;
+      end
+  end
+  $display("Result @ %t nonce = %d %h", $realtime, good_enough, good_enough);
+  $display("SHA3-1600 hasher GOOD");
+  $finish;
+end
+  
+endmodule
