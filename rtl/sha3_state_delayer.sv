9e1b5d57f3d87e49ae47848999392df0c4c58487
MaxDZ8 <MassimoDZ8@gmail.com>
2020-06-14 11:15:05 +0200

    Tried some configurations and stuck with xpm_fifo_sync. Now I exceed LUTRAM as well, fifos have too many bits (16 entries minimum)

--- rtl/sha3_state_delayer.sv
+++ rtl/sha3_state_delayer.sv
@@ -2,6 +2,13 @@
 
 // A bunch of buffers to delay 25 ulongs a few clocks!
 module sha3_state_delayer #(
+    // Some values:
+    // 0- just rename the wires
+    // 1- FF-based delay
+    // 2- FF-based delay
+    // 3..16- 'generic' delay based on FIFO
+    // >16 FAIL
+    // <0 FAIL 
     DELAY = 4
 )(
     input clk,
@@ -54,36 +61,54 @@ else if (DELAY == 2) begin : iobuff // copy input buffer to output buffer
     );
 end
 else begin : generically // one input, one output and something between
-    longint unsigned buff[DELAY][5][5];
-    for (comp = 0; comp < 5; comp++) begin
-        // let's start with the inputs.
-        always_ff @(posedge clk) if (sample) begin
-            buff[0][0][comp] <= isa[comp];
-            buff[0][1][comp] <= isb[comp];
-            buff[0][2][comp] <= isc[comp];
-            buff[0][3][comp] <= isd[comp];
-            buff[0][4][comp] <= ise[comp];
-        end
-        // the in-between shift
-        for (genvar lateness = 0; lateness < DELAY - 1; lateness++) begin
-            always_ff @(posedge clk) begin
-                buff[lateness + 1][0][comp] <= buff[lateness][0][comp]; 
-                buff[lateness + 1][1][comp] <= buff[lateness][1][comp]; 
-                buff[lateness + 1][2][comp] <= buff[lateness][2][comp]; 
-                buff[lateness + 1][3][comp] <= buff[lateness][3][comp]; 
-                buff[lateness + 1][4][comp] <= buff[lateness][4][comp]; 
-            end
-        end
-        assign oda[comp] = buff[DELAY - 1][0][comp];
-        assign odb[comp] = buff[DELAY - 1][1][comp];
-        assign odc[comp] = buff[DELAY - 1][2][comp];
-        assign odd[comp] = buff[DELAY - 1][3][comp];
-        assign ode[comp] = buff[DELAY - 1][4][comp];
-    end
+    
+    bit[(DELAY - 2):0] delay = 0;
+    always_ff @(posedge clk) delay <= { delay[(DELAY - 3):0], sample };
+    assign pullout = delay[DELAY - 2];
 
-    bit[(DELAY - 1):0] delayed_good = 0;
-    always_ff @(posedge clk) delayed_good <= { delayed_good[(DELAY - 2):0], sample };
-    assign good  = delayed_good[DELAY - 1];
+    wire[63:0] consolidated[25];
+    for (genvar comp = 0; comp < 5; comp++) begin
+        assign consolidated[0 * 5 + comp] = isa[comp]; 
+        assign consolidated[1 * 5 + comp] = isb[comp]; 
+        assign consolidated[2 * 5 + comp] = isc[comp]; 
+        assign consolidated[3 * 5 + comp] = isd[comp]; 
+        assign consolidated[4 * 5 + comp] = ise[comp]; 
+    end
+    wire[63:0] giving[25];
+    for (genvar el = 0; el < 25; el++) begin : delayer
+        // I can't get to infer LUTRAM so I'll just put there an XPM and call it day.
+        // Plus, using an XPM (hardware?) fifo can harden the delay counter itself :-)
+        wire[63:0] taking = consolidated[el];
+        xpm_fifo_sync #(
+            .FIFO_MEMORY_TYPE("auto"), // default, let vivado choose
+            .FIFO_READ_LATENCY(1), // default, I'll read one clock first for best timing
+            .FIFO_WRITE_DEPTH(16), // hopefully less, this is the minimum allowed by macro
+            .READ_DATA_WIDTH(64), // default 32
+            .SIM_ASSERT_CHK(1),        // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+            .USE_ADV_FEATURES("0000"), // features: nothing
+            .WRITE_DATA_WIDTH(64) // default 32
+        )
+        xpm_fifo (
+          .wr_en(sample), .din(taking),
+          .rd_en(pullout), .dout(giving[el]),
+          .rst(1'b0), // well... that's weird, but I just keep it flowing.
+          .sleep(1'b0), // as from documentation. It literally reads: "Tie to 1'b0". ^_^!
+          .wr_clk(clk),
+          
+          .injectdbiterr(1'b0), .injectsbiterr(1'b0)
+        );
+    end
+    for (genvar comp = 0; comp < 5; comp++) begin
+        assign oda[comp] = giving[0 * 5 + comp]; 
+        assign odb[comp] = giving[1 * 5 + comp]; 
+        assign odc[comp] = giving[2 * 5 + comp]; 
+        assign odd[comp] = giving[3 * 5 + comp]; 
+        assign ode[comp] = giving[4 * 5 + comp]; 
+    end
+   
+    bit buffo = 1'b0;
+    always_ff @(posedge clk) buffo <= pullout;
+    assign good = buffo;
 end
 
 endmodule
